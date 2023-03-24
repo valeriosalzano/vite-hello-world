bcb93cb854c899c2aed592f7078797ee0250deba
Valerio Salzano <vlr.slzn@gmail.com>
2023-03-24 17:02:57 +0100

    renamed components

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,18 @@
 <script>
-  import MainHeading from './components/MainHeading.vue';
-  import BonusJumbotron from './components/BonusJumobtron.vue';
+  import AppTitle from './components/AppTitle.vue';
+  import AppHero from './components/AppHero.vue';
   export default {
 
     components : {
-      MainHeading,
-      BonusJumbotron,
+      AppTitle,
+      AppHero,
     }
   }
 </script>
 
 <template>
-  <MainHeading/>
-  <BonusJumbotron/>
+  <AppTitle/>
+  <AppHero/>
   <main>
     <h2>Heading2</h2>
     <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Natus nihil a magnam amet atque, earum sapiente assumenda cum dolore ullam vero? Tempore illo officiis nesciunt corrupti deserunt sit natus dolorem id non pariatur, tenetur modi fugit quo adipisci praesentium. Totam, repellendus enim? Voluptatibus aliquam nobis inventore, velit aliquid pariatur nesciunt corporis, eum quidem doloribus minus nihil, ullam deserunt optio ipsum eveniet modi voluptatum! Hic nobis neque quod, delectus cumque, dolores exercitationem totam quas, deserunt minima enim quos officia aut! Velit rem at nobis aliquid autem! Voluptatem enim commodi, corrupti, accusantium cumque laborum officiis atque dolores fugit cupiditate, animi nobis quis!</p>
